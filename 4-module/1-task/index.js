f9167732745f9a008ecb949829a5614fde5a6292
volkov-developer <sergey.volkov.freelancer@gmail.com>
2021-11-08 02:22:28 +0200

    4-module tasks

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const list = document.createElement('ul');
+
+  friends.forEach(friend => {
+    const item = document.createElement('li');
+
+    item.innerText = `${friend.firstName} ${friend.lastName}`;
+
+    list.append(item);
+  });
+
+  return list;
 }
