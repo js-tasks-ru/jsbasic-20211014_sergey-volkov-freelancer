78fd4d130a88d8bc55d332d4d4769161e059903d
volkov-developer <sergey.volkov.freelancer@gmail.com>
2021-11-17 16:41:36 +0200

    6-module 1-task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,69 @@
  *
  */
 export default class UserTable {
-  constructor(rows) {
+  constructor(data) {
+    this.data = data;
+    this.render();
+  }
+
+  render() {
+    this.elem = document.createElement('div');
+    this.elem.innerHTML = `
+      <table>
+          <thead>
+              <tr>
+                  <th>Имя</th>
+                  <th>Возраст</th>
+                  <th>Зарплата</th>
+                  <th>Город</th>
+                  <th></th>
+              </tr>
+          </thead>
+          <tbody>
+
+          </tbody>
+      </table>
+    `;
+
+    this.fillTable();
+  }
+
+  fillTable() {
+    const tableBody = this.elem.querySelector('tbody');
+
+    for (let dataRow of this.data) {
+      const row = this.createRow(dataRow);
+      tableBody.append(row);
+    }
+
+    function onRemoveButtonClick(evt) {
+      if (evt.target.tagName === 'BUTTON') {
+        evt.target.closest('tr').remove();
+      }
+    }
+
+    tableBody.addEventListener('click', onRemoveButtonClick);
+  }
+
+  createRow(obj) {
+    const row = document.createElement('tr');
+
+    for (let prop in obj) {
+      const cell = this.createCell(obj[prop]);
+      row.append(cell);
+    }
+
+    const button = document.createElement('td');
+    button.innerHTML = `<button>X</button>`;
+    row.append(button);
+
+    return row;
+  }
+
+  createCell(value) {
+    const cell = document.createElement('td');
+    cell.innerText = value;
+
+    return cell;
   }
 }
